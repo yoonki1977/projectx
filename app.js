8d52629b4d5bfc090978b6cd3495c371f70914dc
Wonseok Chae <wochae@microsoft.com>
2017-09-21 02:54:08 -0700

    fix file name

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 router.get('/people', function(req, res) {
     var people = [
-        {name: 'water', url: 'assets/fa2ri.png'},
+        {name: 'water', url: 'assets/fa2ri.jpg'},
         {name: 'newthink', url: 'assets/Happyday.png'},
         {name: 'wschae', url: 'assets/wschae.jpg'},
         {name: 'eyyoun', url: 'assets/eyyoun.gif'},
