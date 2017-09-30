6f77c76f96558b4b6012335f3e5ac5ea89c1dc53
Wonseok Chae <wochae@microsoft.com>
2017-09-30 16:19:46 -0700

    Fix link test

--- tests/src/test.js
+++ tests/src/test.js
@@ -31,13 +31,32 @@ module.exports = {
         .assert.containsText('#lists', 'Add more tests');
 
         // Add "Learn how to use git". Now the list has "Learn how to use git".
-        browser.assert.fail('Finish the test');
+        browser
+        .waitForElementVisible('input[type=text]', 1000)
+        .setValue('input[type=text]', 'Learn how to use git')
+        .waitForElementVisible('button[id=addItem]', 1000)
+        .click('button[id=addItem]')
+        .pause(1000)
+        .assert.containsText('#lists', 'Learn how to use git');
 
         // click "Team" link to check if the title is "Team"
+        browser
+        .useXpath()
+        .waitForElementVisible("//a[text()='Team']", 1000)
+        .click("//a[text()='Team']")
+        .useCss()
+        .assert.title('Project X Team');
 
         // click "TODO" link back to check if the title is "TODO"
-        
+        browser
+        .useXpath()
+        .waitForElementVisible("//a[text()='TODO']", 1000)
+        .click("//a[text()='TODO']")
+        .useCss()
+        .assert.title('TODO list');     
+
         // Check the list still has "Add more tests" and "Learn how to use git"
+        browser.assert.fail('Finish the test');
 
         // remove "Add more tests" and check if "Add more tests" is gone
         
