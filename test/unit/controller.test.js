17e225bc841028ac75bb3ef05fa9a43f24e0d680
Wonseok Chae <wochae@microsoft.com>
2017-09-30 12:58:27 -0700

    add controller test

---
+++ test/unit/controller.test.js
@@ -0,0 +1,9 @@
+describe('MainController', function(){
+    beforeEach(module('demo'));
+    it('Should have a todo list', inject(function($controller){
+        var myController = $controller('MainController');
+
+        myController.list.should.not.empty;
+        myController.list.should.contain('Finish First Push');
+    }));
+});
