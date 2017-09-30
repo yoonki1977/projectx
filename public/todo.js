9130979f904caff7ea708893dbe5c21aea4781d9
Wonseok Chae <wochae@microsoft.com>
2017-09-30 13:43:01 -0700

    add an item into a todo list

--- public/todo.js
+++ public/todo.js
@@ -1,4 +1,7 @@
 var demoApp = angular.module('demo', []);
 demoApp.controller('MainController', ['$scope', function($scope){
     $scope.list = ['Finish First Push'];
+    $scope.addItem = function(item) {
+        $scope.list.push(item);
+    };
 }]);
--- test/unit/controller.test.js
+++ test/unit/controller.test.js
@@ -6,5 +6,13 @@ describe('MainController', function(){
 
         scope.list.should.not.empty;
         scope.list.should.contain('Finish First Push');
-    }));    
+    }));
+
+    it('Should add a new item', inject(function($controller){
+        var scope = {};
+        var myController = $controller('MainController', {$scope:scope});
+
+        scope.addItem("Read 'No Silver Bullet");
+        scope.list.should.contain("Read 'No Silver Bullet");
+    }));      
 });
